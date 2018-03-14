af82c3dd583c126c4c450ef6b83ce0f6ea68bdb5
James Bews <jbews11@gmail.com>
2018-03-14 13:20:43 -0600

    Complete.

--- Call It Local/ClientApplicationMVC/Controllers/AuthenticationController.cs
+++ Call It Local/ClientApplicationMVC/Controllers/AuthenticationController.cs
@@ -4,7 +4,7 @@ using Messages.NServiceBus.Commands;
 using Messages.DataTypes;
 using Messages.ServiceBusRequest;
 using Messages.ServiceBusRequest.Authentication.Requests;
-
+using System.Web;
 using System.Web.Mvc;
 
 namespace ClientApplicationMVC.Controllers
@@ -24,22 +24,71 @@ namespace ClientApplicationMVC.Controllers
             return View("Index");
         }
 
-        public ActionResult LogIn()
+        public ActionResult LogIn(string username, string password)
         {
-            ViewBag.LoginResponse = "TEST";
-            return View("Index"); //Change to display new page
+
+            LogInRequest request = new LogInRequest(username, password);
+
+            ServiceBusResponse response;
+            ServiceBusConnection connection = ConnectionManager.getConnectionObject(Globals.getUser());
+            if (connection == null)
+            {
+                response = ConnectionManager.sendLogIn(request);
+            }
+            else
+            {
+                response = connection.sendLogIn(request);
+            }
+            if (response.result)
+            {
+                ViewBag.LoginResponse = "Login Successful";
+            }else
+            {
+                ViewBag.LoginResponse = "Invalid Username or Password";
+            }
+            
+            return View("Index");
+
         }
 
         public ActionResult CreateAccount()
         {
+            
+
             return View("CreateAccount");
         }
 
-        public ActionResult CreationConfirmationPage()
+        public ActionResult CreationConfirmationPage(string username, string email, string password,
+            string address, string phonenumber, AccountType users)
         {
+            CreateAccount account = new CreateAccount();
+            account.username = username;
+            account.password = password;
+            account.address = address;
+            account.phonenumber = phonenumber;
+            account.email = email;
+            account.type = users;
+            CreateAccountRequest request = new CreateAccountRequest(account);
+            ServiceBusResponse response;
+            ServiceBusConnection connection = ConnectionManager.getConnectionObject(Globals.getUser());
+            if(connection == null)
+            {
+                response = ConnectionManager.sendNewAccountInfo(request);
+            } else
+            {
+                response = connection.sendNewAccountInfo(request);
+            }
+            if (response.result)
+            {
+                return View("Index");
+            } else
+            {
+                ViewBag.CreationMessage = response.response;
+                return View("CreateAccount");
+            }
+            
 
-            ViewBag.Confirmation = "Account Created";
-            return View("CreationConfirmationPage");
+            
         }
 		
 		//This class is incomplete and should be completed by the students in milestone 2
--- Call It Local/ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
+++ Call It Local/ClientApplicationMVC/Scripts/Authentication/AuthenticationFunctions.js
@@ -1,12 +1,36 @@
 ﻿// To be completed by students in milestone 2
 
 function validatecredentialForm() {
-    //must validate the data
+    if (credentialForm.username.value === "" || credentialForm.password.value === "") {
+        return false;
+    } else {
+        return true;
+    }
 
 }
 
 function validateCreationForm() {
-    //must error check new user creation
 
-    return view("CreationConfirmationPage");
+    if (accountCreationForm.username.value === "" || accountCreationForm.email.value === ""
+        || accountCreationForm.password.value === "" || accountCreationForm.confirmPassword.value === ""
+        || accountCreationForm.address.value === "" || accountCreationForm.users.value === "Select Type"
+        || accountCreationForm.phonenumber.value === "") {
+        return false;
+    }
+    if (accountCreationForm.password.value != accountCreationForm.confirmPassword.value) {
+        return false;
+    }
+    var email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!email.test(String(accountCreationForm.email.value).toLowerCase())) {
+        window.alert("Invalid E-mail. Try Again!");
+
+        return false;
+    }
+    var phone = /^\d{3}-?\d{4}$/;
+    if (!phone.test(String(accountCreationForm.phonenumber.value).toLowerCase())) {
+        window.alert("Invalid Phone Number. Format should be xxx-xxxx");
+
+        return false;
+    }
+    return true;
 }
