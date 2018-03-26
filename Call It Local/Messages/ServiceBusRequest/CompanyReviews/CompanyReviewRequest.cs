f94b07a29f7236d3125baf13ad9b726e74fd087d
kendal blais <kendalblais@gmail.com>
2018-03-26 10:28:35 -0600

    Added company review requests and responses as well as companyreview datatype for service bus

---
+++ Call It Local/Messages/ServiceBusRequest/CompanyReviews/CompanyReviewRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messages.ServiceBusRequest.CompanyReviews
+{
+    [Serializable]
+    public class CompanySearchRequest : ServiceBusRequest
+    {
+        public CompanySearchRequest(string companyName)
+            : base(Service.CompanyReviews)
+        {
+            this.companyName = companyName;
+        }
+
+        /// <summary>
+        /// Information used to search the database for companies
+        /// </summary>
+        public string companyName;
+    }
+}
